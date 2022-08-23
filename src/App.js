6cb92786d0103630967e164bb3a39e1af8515c4a
Basil Ohuruogu <ohuruogubasil@gmail.com>
2022-08-23 22:52:40 +0100

    final commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,96 @@
 import './App.css';
 import Book from './Book';
 
-const firstBook = {
+
+export const books = [
+
+  {
+    id:1,
     img: 'https://images-na.ssl-images-amazon.com/images/I/61Bdp7XZhDL._AC_UL600_SR300,400_.jpg',
     title: 'The Silent Patient',
     author: 'Alex Michaelides',
-    job: 'Developer'
-}
-
-const secondBook = {
+    
+  },
+  
+  {
+    id:2,
     img: 'https://images-na.ssl-images-amazon.com/images/I/819mN6mU4xL._AC_UL600_SR300,400_.jpg',
     title: 'Wrong Place Wrong Time',
     author: 'Gillian McAllister',
-    job: 'Developer'
-}
+    
+  },
+
+  {
+    id:3,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81Ekpv7AwKL._AC_UL600_SR300,400_.jpg',
+    title: 'My First Learn-to-Write',
+    author: 'Crystal Radke',
+    
+  },
+  {
+    id:4,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/61Bdp7XZhDL._AC_UL600_SR300,400_.jpg',
+    title: 'The Silent Patient',
+    author: 'Alex Michaelides',
+  
+  },
+  
+  {
+    id:5,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/819mN6mU4xL._AC_UL600_SR300,400_.jpg',
+    title: 'Wrong Place Wrong Time',
+    author: 'Gillian McAllister',
+    
+  },
+
+  {
+    id:6,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81Ekpv7AwKL._AC_UL600_SR300,400_.jpg',
+    title: 'My First Learn-to-Write',
+    author: 'Crystal Radke',
+    
+  },
+  {
+    id:7,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/61Bdp7XZhDL._AC_UL600_SR300,400_.jpg',
+    title: 'The Silent Patient',
+    author: 'Alex Michaelides',
+    
+  },
+  
+  {
+    id:8,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/819mN6mU4xL._AC_UL600_SR300,400_.jpg',
+    title: 'Wrong Place Wrong Time',
+    author: 'Gillian McAllister',
+   
+  },
+
+    {id:9,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81Ekpv7AwKL._AC_UL600_SR300,400_.jpg',
+    title: 'My First Learn-to-Write',
+    author: 'Crystal Radke',
+    
+  },
+
+]
+
+
 
 
 function App() {
   return (
     <div className='App'>
-      <Book img={firstBook.img} title={firstBook.title} author={firstBook.author} job={firstBook.job}>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis deleniti non incidunt eaque, recusandae voluptas.</p>
-      </Book>
-      <Book img={secondBook.img} title={secondBook.title} author={secondBook.author} job={secondBook.job} >
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis deleniti non incidunt eaque, recusandae voluptas.</p>
-      </Book>
-     
+      {books.map((book) => {
+        // const { img, title, author} = book
+        return (
+          // <Book img={img} title={title} author={author}  />
+          <Book key={book.id} {...book} />
+        )
+        })}
     </div>
   );
 }
+
 
 export default App;
--- src/Book.js
+++ src/Book.js
@@ -2,15 +2,24 @@ import React from 'react'
 import './App.css';
 import './book.css'
 
+
+
+
 export default function Book(props) {
-const {img, title, author, job, children} = props
+    const { img, title, author} = props
+    
+    //attribute, eventHandler
+    // onClick, onMouseOver
+    const clickHandler =() => {
+        alert('Hello world')
+    } 
     return (
         <article className='book'>
             <img src={img} alt="" /> 
             <h2>{title}</h2>
-            <h3>{author}</h3>  
-            {children}
-            <p>{job}</p>
+            <h3>{author }</h3>
+           
+            <button type='button' onClick={clickHandler}>Buy now</button>
         </article>
   )
 }
